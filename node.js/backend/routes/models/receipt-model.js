bfb80bcb5b0e614980f90d15d143abccbf40a6c3
fazil2003 <mohamedfazil463@gmail.com>
2023-03-09 17:45:50 +0530

    Created the Receipts Schema.

---
+++ node.js/backend/routes/models/receipt-model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const db = require("./connect-db");
+
+// Create an schema
+var receiptsSchema = new mongoose.Schema({
+    receipt_id: Number,
+    created_timestamp: Number,
+    updated_timestamp: Number,
+    shop_id: Number,
+    is_processed: Boolean
+});
+
+var receiptsModel = mongoose.model('receipts', receiptsSchema);
+module.exports = receiptsModel;
